83963e1de3d395bb139f92dc78448cd0eea0383b
Ramjeet Singh <ramjeet2k2@gmail.com>
2022-11-22 23:52:05 +0530

    updated login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import React from 'react';
+import background from './loginBack.jpg'
 
 export default function Login(props) {
     const [inputs, setInputs] = useState({});
@@ -27,30 +28,39 @@ export default function Login(props) {
     }
 
   return(
-    <form>
-      {(props.error !== "")? (<div className='error'>{props.error}</div>) : ""}
+    <div className='text-dark' style={{textAlign:"center", flexDirection:"column", alignItems:"center", justifyContent: "center", display: "flex", backgroundImage:`url(${background})`, padding:"120px 0 90px 0", height:"100vh"}}>
+      {/* <p style={{display:"flex"}}>wnsiw</p> */}
+      <h1 style={{fontFamily:"serif"}}>Enter credentials to login!</h1>
 
-      <label> Enter username:
-        <input 
-            type="text" 
-            name="username" 
-            value={inputs.username || ""} 
-            onChange={handleChange}
-        />
-      </label>
+        <form style={{display:"flex", flexDirection:"column"}}>
+          {(props.error !== "")? (<div className='error' style={{color:"red"}}>{props.error}</div>) : ""}
 
-      <label> Enter password:
-        <input 
-            type="password" 
-            name="password" 
-            value={inputs.password || ""} 
-            onChange={handleChange}
-        />
-      </label>
+          <label style={{margin:"50px 0 20px 0", fontWeight:"bold"}}> Enter username: 
+            <input style={{background:"transparent"}}
+                type="text" 
+                name="username" 
+                value={inputs.username || ""} 
+                onChange={handleChange}
+            />
+          </label>
 
-      <div>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-      </div>
-    </form>
+          <label style={{margin:"0 0 30px 0", fontWeight:"bold"}}> Enter password:
+            <input style={{background:"transparent"}}
+                type="password" 
+                name="password" 
+                value={inputs.password || ""} 
+                onChange={handleChange}
+            />
+          </label>
+
+          <div>
+            <button className='btn btn-outline-dark' type="submit" onClick={handleSubmit}>Login</button>
+          </div>
+        </form>
+      {/* <div style={{display:"flex", flexDirection:"column", justifyContent: "center", height:"500px", width:"30%", backgroundColor:"#383838", opacity:"0.3"}}>
+        
+      </div> */}
+    </div>
+    
   )
 }
